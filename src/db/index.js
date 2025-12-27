a40de359f989ee1f5f1de71785b9b829bfe880d8
ManishYadav-123 <m69906241@gmail.com>
2025-12-27 15:56:06 +0530

    db connected successfully

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
-const connectDB =(async ()=>{
+const connectDB =async ()=>{
+    if (!process.env.MONGODB_URL) {
+    console.error("Error: MONGODB_URL is not defined in .env");
+    process.exit(1);
+    }
     try{
         const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     }
     catch(error){
-        console.log("MONGODB CONNECTION ERROR",error);
+        console.log("MONGODB CONNECTION FAILED",error);
     }
-})
+}
 
-export default connectDB
+export default connectDB;
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from './constants'   
-import {connectDB} from './constants'
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+dotenv.config({
+    path: './.env'
+})
 connectDB()
